5c77466e11f5dd8c72d8125cc425e839046b7332
mycroft440 <alquimistadodetalhe@gmail.com>
2025-08-30 11:33:21 -0300

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
