48b6006edf376ac8fc533f2d06536799d349e047
mycroft440 <alquimistadodetalhe@gmail.com>
2025-08-29 22:12:10 -0300

    Update Packet.kt

--- app/src/main/java/com/example/droidtunnel/Packet.kt
+++ app/src/main/java/com/example/droidtunnel/Packet.kt
@@ -1,1 +1,22 @@
-[Immersive content redacted for brevity.]
+package com.example.droidtunnel
+
+import java.net.InetAddress
+import java.nio.ByteBuffer
+
+data class Packet(
+    val ipHeader: IpHeader,
+    val transportHeader: TransportHeader,
+    val data: ByteBuffer
+) {
+    interface IpHeader {
+        val version: Int
+        val protocol: Int
+        val sourceAddress: InetAddress
+        val destinationAddress: InetAddress
+    }
+
+    interface TransportHeader {
+        val sourcePort: Int
+        val destinationPort: Int
+    }
+}
