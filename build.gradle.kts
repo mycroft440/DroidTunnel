27d1710bad0ab903e0aac8df4e3ab68dd169e82a
mycroft440 <alquimistadodetalhe@gmail.com>
2025-09-01 08:40:39 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-// Top-level build file where you can add configuration options common to all 
-// sub-projects/modules.
+// Ficheiro de build de nível superior onde pode adicionar opções de configuração
+// comuns a todos os subprojetos/módulos.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    // Atualizado para a versão estável mais recente do Android Gradle Plugin
+    id("com.android.application") version "8.4.1" apply false
+    // Mantém a versão do Kotlin alinhada com as dependências do Compose
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
